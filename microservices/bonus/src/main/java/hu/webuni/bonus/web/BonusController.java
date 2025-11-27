89f986eb11c073556598d0db6aa39dfb4c26be28
imre-gabor <gabor@aut.bme.hu>
2025-11-27 19:42:17 +0100

    add authorization to bonus endpoint

--- microservices/bonus/src/main/java/hu/webuni/bonus/web/BonusController.java
+++ microservices/bonus/src/main/java/hu/webuni/bonus/web/BonusController.java
@@ -7,6 +7,7 @@ import hu.webuni.bonus.service.BonusService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
@@ -22,6 +23,7 @@ public class BonusController implements BonusApi {
     BonusService bonusService;
     
     @Override
+    @PreAuthorize("#user == authentication.name")
     public double getPoints(String user) {
         return bonusRepository.findById(user)
                 .orElseGet(Bonus::new)
